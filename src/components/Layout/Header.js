7ad1dbeeb754ac2e46aa5a9074e7920daaa26f58
JanSobeslav <sobeslav.jan@seznam.cz>
2022-02-24 11:17:38 +0100

    header button comp, add icon

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import classes from './Header.module.css';
 import image from '../../assets/meals.jpg'; //lze importovat obrázek -> důležité je napsat koncovku ".jpg"
+import HeaderCartButton from './HeaderCartButton';
 
 function Header(props) {
     return (
@@ -8,7 +9,7 @@ function Header(props) {
          <React.Fragment>  {/* fragment se využívá proto, aby nevznikaly obalové divy -> zpomalení aplikace */}
             <header className={classes.header}> {/*CSS Module přistoupení k názvu třídy */}
                 <h1>ReactMeals</h1>
-                <button>Cart</button>
+                <HeaderCartButton />
             </header>
             <div className={classes['main-image']}>  {/*CSS Module přistoupení k názvu pokud je nějak rozdělenen např. '-' */}
                 <img src={image} alt="Table full of delicious food!" />
