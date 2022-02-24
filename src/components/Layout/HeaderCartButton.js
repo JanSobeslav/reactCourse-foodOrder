b3a6e4cc941b492eb3afab328ece300066eb7c45
JanSobeslav <sobeslav.jan@seznam.cz>
2022-02-24 15:38:28 +0100

    reducer

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,13 +1,19 @@
 import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css';
+import CartContext from '../../store/cart-context';
+import { useContext } from 'react';
 
 function HeaderCartButton(props) {
+    const cartCtx = useContext(CartContext);
+    const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+        return curNumber + item.amount;
+    }, 0); //vrátí počet položek v košíku
     return (
         // funkce showCartHandler() pod názvem onClick je přijata a vložena do skutečného onClick
         <button className={classes.button} onClick={props.onClick}>
             <span className={classes.icon}><CartIcon /></span>
             <span>Your Cart</span>
-            <span className={classes.badge}>3</span>
+            <span className={classes.badge}>{numberOfCartItems}</span>
         </button>
     );
 }
--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,20 +1,39 @@
+import { useReducer } from "react";
 import CartContext from "./cart-context";
 
+const defaultCartState = { //základní stav -> při vytvoření 
+    items: [],
+    totalAmount: 0
+}
+
+function cartReducer(state, action) {
+    if (action.typeIdentif === 'ADD_ITEM') {
+        const updatedItems = state.items.concat(action.item); //vezme se původní stav "state", vytvoří se nové pole a přidá nová položka action.item (concat)
+        const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+        return {
+            items: updatedItems,
+            totalAmount: updatedTotalAmount
+        };
+    }
+    return defaultCartState;
+}
+
 function CartProvider(props) {
+    const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState);
 
     function addItemToCartHandler(item) {
-
+        dispatchCartAction({ typeIdentif: 'ADD_ITEM', item: item }); //aktualizují se data s identifikátorem akce
     }
 
     function removeItemFromCartHandler(id) {
-
+        dispatchCartAction({ typeIdentif: 'REMOVE_ITEM', id: id }); //aktualizují se data s identifikátorem akce
     }
 
     const cartContext = {
-        items: [],
-        totalAmount: 0,
+        items: cartState.items,
+        totalAmount: cartState.totalAmount,
         addItem: addItemToCartHandler,
-        removeItem: removeItemFromCartHandler 
+        removeItem: removeItemFromCartHandler
     };
     return (
         <CartContext.Provider value={cartContext}>
