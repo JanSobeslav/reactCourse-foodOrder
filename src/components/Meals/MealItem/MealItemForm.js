57733b37ecd078e4148b663bec62f2c45308bfd5
JanSobeslav <sobeslav.jan@seznam.cz>
2022-02-24 15:55:33 +0100

    useRef to comp - get values from input

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,10 +1,26 @@
+import { useRef, useState } from 'react';
 import Input from '../../UI/Input';
 import classes from './MealItemForm.module.css';
 
 function MealItemForm(props) {
+    const [amountIsValid, setAmountIsValid] = useState(true);
+    const amountInputRef = useRef();
+
+    function submitHandler(event) {
+        event.preventDefault();
+
+        const enteredAmount = amountInputRef.current.value; //získaná hodnota z Input.js ref (je to vždy string!)
+        const enteredAmountNum = +enteredAmount; //konverze na číslo
+
+        if (enteredAmount.trim().length === 0 || enteredAmountNum < 1 || enteredAmountNum > 5) {
+            setAmountIsValid(false);
+            return;
+        }
+    }
+
     return (
-        <form className={classes.form}>
-            <Input label="Amount" input={{
+        <form className={classes.form} onSubmit={submitHandler}>
+            <Input label="Amount" ref={amountInputRef} input={{ //ref nelze použít v komponentě -> přenáší se jako props
                 id: props.id,
                 type: 'number',
                 min: '1',
@@ -13,6 +29,7 @@ function MealItemForm(props) {
                 defaultValue: '1'
             }}/> {/* vkládám do props objekt, se kterým se pracuje v Input.js */}
             <button>+ Add</button>
+            {!amountIsValid && <p>Please enter a valid amount (1-5).</p>}
         </form>
     );
 }
--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -1,15 +1,16 @@
+import React from 'react';
 import classes from './Input.module.css';
 
-function Input(props) {
+const Input = React.forwardRef((props, ref) => { //přijatý props ref lze využít jako skutečný ref pomocí React.forwardRef()
     return (
         <div className={classes.input}>
             <label htmlFor={props.input.id}>{props.label}</label>
-            <input {...props.input} />
+            <input ref={ref} {...props.input} /> {/* získaný ref vložíme do skutečného ref */}
             {/* ...props.input zajistí, že se vše z objektu input dostane jako atribut => 
             <input id='' type='' min='' max='' atd. /> 
             musí to být atributy, které <input /> opravdu má! */}
         </div>
     );
-}
+});
 
 export default Input;
