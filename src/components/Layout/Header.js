04a4ada5943e8f778b082a8739334b8689639559
JanSobeslav <sobeslav.jan@seznam.cz>
2022-02-24 11:05:00 +0100

    vytvoření základní struktury projektu, header

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import classes from './Header.module.css';
+import image from '../../assets/meals.jpg'; //lze importovat obrázek -> důležité je napsat koncovku ".jpg"
+
+function Header(props) {
+    return (
+        
+         <React.Fragment>  {/* fragment se využívá proto, aby nevznikaly obalové divy -> zpomalení aplikace */}
+            <header className={classes.header}> {/*CSS Module přistoupení k názvu třídy */}
+                <h1>ReactMeals</h1>
+                <button>Cart</button>
+            </header>
+            <div className={classes['main-image']}>  {/*CSS Module přistoupení k názvu pokud je nějak rozdělenen např. '-' */}
+                <img src={image} alt="Table full of delicious food!" />
+            </div>
+        </React.Fragment>
+    );
+}
+
+export default Header;
