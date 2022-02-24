67868dd48a2034b4a32fbd1c893b18e6b93a0948
JanSobeslav <sobeslav.jan@seznam.cz>
2022-02-24 16:48:22 +0100

    add CartItem bind add, remove

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,5 +1,6 @@
 import { useContext } from 'react';
 import CartContext from '../../store/cart-context';
+import CartItem from './CartItem.js';
 import Modal from '../UI/Modal';
 import classes from './Cart.module.css';
 
@@ -7,7 +8,18 @@ function Cart(props) {
     const cartCtx = useContext(CartContext);
     const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
     const hasItems = cartCtx.items.length > 0;
-    const cartItems = <ul className={classes['cart-items']}>{cartCtx.items.map((item) => <li>{item.name}</li>)}</ul>;
+
+    function cartItemAddHandler(item) {
+
+    }
+
+    function cartItemRemoveHandler(id) {
+
+    }
+
+    const cartItems = <ul className={classes['cart-items']}>{cartCtx.items.map((item) =>
+        <CartItem key={item.id} name={item.name} amount={item.amount} price={item.price} 
+        onRemove={cartItemRemoveHandler.bind(null, item.id)} onAdd={cartItemAddHandler.bind(null, item)} />)}</ul>;
 
     return (
         <Modal onClose={props.onClose}> {/* přijetí funkce hideCartHandler() jako onClose a odeslání do Modal comp pod názvem onClose */}
