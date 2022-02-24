8c141d54c4c8d31ab646b8c03f7f2156fd5d9fd7
JanSobeslav <sobeslav.jan@seznam.cz>
2022-02-24 16:25:46 +0100

    display total amount in cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,15 +1,19 @@
+import { useContext } from 'react';
+import CartContext from '../../store/cart-context';
 import Modal from '../UI/Modal';
 import classes from './Cart.module.css';
 
 function Cart(props) {
-    const cartItems = <ul className={classes['cart-items']}>{[{ id: 'c1', name: 'Sushi', amount: 2, price: 12.99 }].map((item) => <li>{item.name}</li>)}</ul>;
+    const cartCtx = useContext(CartContext);
+    const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+    const cartItems = <ul className={classes['cart-items']}>{cartCtx.items.map((item) => <li>{item.name}</li>)}</ul>;
 
     return (
         <Modal onClose={props.onClose}> {/* přijetí funkce hideCartHandler() jako onClose a odeslání do Modal comp pod názvem onClose */}
             {cartItems}
             <div className={classes.total}>
                 <span>Total Amount</span>
-                <span>35.92</span>
+                <span>{totalAmount}</span>
             </div>
             <div className={classes.actions}>
                 {/* přijetí funkce hideCartHandler() jako onClose a vložení do funkce tlačítka onClick */}
